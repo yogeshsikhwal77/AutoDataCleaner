Fixed scaling bug and made structuring universal

src/inspection.py
@@ -46,8 +46,11 @@ class DataInspector:
 
         for col in cat_cols:
             if self.df[col].isnull().sum() > 0:
-                self.df[col] = self.df[col].fillna(self.df[col].mode()[0])
-        print(self.df.isnull().sum())
+                mode_vals = self.df[col].mode()
+                if not mode_vals.empty:
+                    self.df[col] = self.df[col].fillna(mode_vals[0])
+                else:
+                    self.df.drop(columns=[col],inplace=True)
 
         return self.df
     
@@ -58,18 +61,18 @@ class DataInspector:
             p_25 = self.df[col].quantile(0.25)
             p_75 = self.df[col].quantile(0.75)
             iqr = p_75 - p_25
-            max = p_75 + 1.5 * iqr
-            min = p_25 - 1.5 * iqr
+            upper_bound = p_75 + 1.5 * iqr
+            lower_bound = p_25 - 1.5 * iqr
 
-            outliers = ((self.df[col] < min) | (self.df[col] > max)).sum()
+            outliers = ((self.df[col] < lower_bound) | (self.df[col] > upper_bound)).sum()
             outlier_capped += outliers
 
             self.df[col] = np.where(
-                self.df[col] > max,
-                max,
+                self.df[col] > upper_bound,
+                upper_bound,
                 np.where(
-                    self.df[col] < min,
-                    min,
+                    self.df[col] < lower_bound,
+                    lower_bound,
                     self.df[col]
                 )
             )

src/structuring.py
@@ -6,17 +6,23 @@ class DataStructurer:
     def __init__(self, df: pd.DataFrame):
         self.df = df.copy()
 
-    def date_time(self):
-        time_cols = [col for col in self.df.columns if any(word in col for word in ['Year','Yr','Mo'])]
+    def date_time(self,cyclic_month_cols):
 
-        for col in time_cols:
-            print(f"found temporal feature/date column: {col}")
+        if cyclic_month_cols is None:
+            cyclic_month_cols =[]
+        
 
-            if 'Mo' in col:
+        for col in cyclic_month_cols:
+            if col in self.df.columns:
+                print(f"found temporal feature/date column: {col}")
+
+         
                 self.df[col +'_sin'] = np.sin(2 * np.pi* self.df[col]/12)
                 self.df[col +'_cos'] = np.cos(2 * np.pi* self.df[col]/12)
                 self.df.drop(columns=[col],inplace = True)
                 print(f"coverted '{col}' into cyclic sin/cos waves")
+            else:
+                print("temporal coloum not fouund in dataset")
 
         return self.df
         
@@ -55,7 +61,7 @@ if __name__ == "__main__":
 
         structure =DataStructurer(fake_data)
 
-        structure.date_time()
+        structure.date_time('datacoloum')
 
         structure.feature_splitting('OverallQual','GrLivArea','QualXarea',operation='multiply')
 

src/transformations.py
@@ -30,15 +30,17 @@ class DataTransformer:
 
         return self.df
     
-    def scale_featurs(self):
+    def scale_featurs(self,fitted_scaler=None):
         num_col = self.df.select_dtypes(include=np.number).columns.tolist()
-        scaler = StandardScaler()
-
         num_col_only = [col for col in num_col if self.df[col].dtype != 'bool']
 
-        self.df[num_col_only] = scaler.fit_transform(self.df[num_col_only])
-
-        return self.df
+        if fitted_scaler is None:
+            scaler = StandardScaler()
+            self.df[num_col_only] = scaler.fit_transform(self.df[num_col_only])
+            return self.df,scaler
+        else:
+            self.df[num_col_only] = scaler.fit_transform(self.df[num_col_only])
+            return self.df
 
     
 